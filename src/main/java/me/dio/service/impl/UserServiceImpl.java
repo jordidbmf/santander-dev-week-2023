66e49bdcff286b84f5237080043e8686a5202e64
jordidbmf <jordi@zamplex.com.br>
2023-10-13 17:05:11 -0300

    Criação do Controller e do Repository.

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package me.dio.service.impl;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        //if(userToCreate.getId() != null && userRepository.existsById(userToCreate.getId())){
+        if(userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
+            throw new IllegalArgumentException("This Account Number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+    
+}
